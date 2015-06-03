64c71c9a2e974c26b2cf1591b56ad5f3cd8485a7
beiyong01 <beiyong01@beiyong01.sx-hl.com>
2015-06-03 20:26:12 +0800

    11111

---
+++ src/com/jmdns/multicast/device/ProtoSocket.java
@@ -0,0 +1,28 @@
+package com.jmdns.multicast.device;
+
+/*
+ * File：ProtoSocket.java
+ *
+ * Copyright (C) 2015 JmdnsService Project
+ * Date：2015年6月3日 下午7:57:01
+ * All Rights SXHL(New Space) Corporation Reserved.
+ * http://www.at-et.cn
+ *
+ */
+
+/**
+ * @description:通过用ProtoBuf这种接口序列化的代码来实现
+ * 
+ * @author: HuJun
+ * @date: 2015年6月3日 下午7:57:01
+ */
+
+public class ProtoSocket implements Runnable {
+    
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
